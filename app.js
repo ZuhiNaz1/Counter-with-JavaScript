60546e92920732da253610b442a85e90fc75952f
ZuhiNaz1 <zuhinaz1518@gmail.com>
2023-06-01 12:46:23 +0100

    menu commit

--- app.js
+++ app.js
@@ -1,82 +1,149 @@
-// local reviews data
-const reviews = [
+//data set
+const menu = [
     {
       id: 1,
-      name: 'susan smith',
-      job: 'web developer',
-      img: 'https://www.course-api.com/images/people/person-1.jpeg',
-      text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+      title: "buttermilk pancakes",
+      category: "breakfast",
+      price: 15.99,
+      img: "./images/item-1.jpeg",
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
     },
     {
       id: 2,
-      name: 'anna johnson',
-      job: 'web designer',
-      img: 'https://www.course-api.com/images/people/person-2.jpeg',
-      text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+      title: "diner double",
+      category: "lunch",
+      price: 13.99,
+      img: "./images/item-2.jpeg",
+      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
     },
     {
       id: 3,
-      name: 'peter jones',
-      job: 'intern',
-      img: 'https://www.course-api.com/images/people/person-4.jpeg',
-      text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+      title: "godzilla milkshake",
+      category: "shakes",
+      price: 6.99,
+      img: "./images/item-3.jpeg",
+      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
     },
     {
       id: 4,
-      name: 'bill anderson',
-      job: 'the boss',
-      img: 'https://www.course-api.com/images/people/person-3.jpeg',
-      text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+      title: "country delight",
+      category: "breakfast",
+      price: 20.99,
+      img: "./images/item-4.jpeg",
+      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    },
+    {
+      id: 5,
+      title: "egg attack",
+      category: "lunch",
+      price: 22.99,
+      img: "./images/item-5.jpeg",
+      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    },
+    {
+      id: 6,
+      title: "oreo dream",
+      category: "shakes",
+      price: 18.99,
+      img: "./images/item-6.jpeg",
+      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    },
+    {
+      id: 7,
+      title: "bacon overflow",
+      category: "breakfast",
+      price: 8.99,
+      img: "./images/item-7.jpeg",
+      desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    },
+    {
+      id: 8,
+      title: "american classic",
+      category: "lunch",
+      price: 12.99,
+      img: "./images/item-8.jpeg",
+      desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    },
+    {
+      id: 9,
+      title: "quarantine buddy",
+      category: "shakes",
+      price: 16.99,
+      img: "./images/item-9.jpeg",
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+    {
+      id: 10,
+      title: "bison steak",
+      category: "dinner",
+      price: 22.99,
+      img: "./images/item-10.jpeg",
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
     },
   ];
 
-//select items
-const img = document.getElementById('person-img');
-const author = document.getElementById('author');
-const job = document.getElementById('job');
-const info = document.getElementById('info');
+  const sectionCenter = document.querySelector('.section-center');
+  const btnContainer = document.querySelector(".btn-container");
 
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const randomBtn = document.querySelector('.random-btn');
+ 
+  
+  //load items
+  window.addEventListener('DOMContentLoaded', function(){
+    displayMenuItems(menu);
+    displayMenuButtons();
+  });
+  function displayMenuItems(menuItems){
+    let displayMenu = menuItems.map(function (item){
+     //console.log(item);
+     return ` <article class="menu-item">
+     <img src=${item.img} class="photo" alt=${item.title}/>
+     <div class="item-info">
+        <header>
+          <h4>${item.title}</h4>
+          <h4 class="price">$${item.price}</h4>
+        </header>
+          <p class="item-text">
+             ${item.desc}
+          </p>
+   </article>`
+    });
+    displayMenu = displayMenu.join("");
+    sectionCenter.innerHTML = displayMenu;
+  };
 
-//set starting item
-let currentItem = 0;
+  function displayMenuButtons(){
+     const categories = menu.reduce(function(values, item){
+      if(!values.includes(item.category)){
+        values.push(item.category);
+      }
+    return values;
 
-//load initial item
-window.addEventListener('DOMContentLoaded', function(){
-  showPerson();
-});
+    },['all']);
+      const categoryBtns = categories.map(function(category){
+        return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`
+      }).join("");
+      btnContainer.innerHTML = categoryBtns;
+      const filterBtns = document.querySelectorAll('.filter-btn');
+      console.log(filterBtns);
+  
+  //filter items 
+  filterBtns.forEach(function(btn){
+    btn.addEventListener("click", function(e) {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem){
+        //console.log(menuItem.category);
+        if(menuItem.category === category){
+        return menuItem;
+      }
+      });
+      //console.log(menuCategory);
+      if(category === 'all'){
+        displayMenuItems(menu);
+      }
+      else{
+        displayMenuItems(menuCategory);
+      }
+    });
+  }); 
 
-//show person based on item
-function showPerson(){
-  const item = reviews[currentItem];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
-}
-
-//show next person
-nextBtn.addEventListener('click', function(){
-    currentItem++;
-    if(currentItem > reviews.length - 1){
-      currentItem = 0;
-    }
-    showPerson();
-});
-//show previous person
-prevBtn.addEventListener('click', function(){
-  currentItem--;
-  if(currentItem > 0){
-    currentItem = reviews.length - 1;
-  }
-  showPerson(currentItem);
-});
-
-//show random person
-randomBtn.addEventListener('click', function(){
-  currentItem = Math.floor(Math.random() * reviews.length);
-  console.log(currentItem);
-  showPerson();
-});
+  }   
