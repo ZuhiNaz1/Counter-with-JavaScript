66eb1de0f5bae3498b2e8d509db4269fec2f11e3
ZuhiNaz1 <zuhinaz1518@gmail.com>
2023-06-05 15:56:57 +0100

    final commit

--- app.js
+++ app.js
@@ -1,71 +1,91 @@
-//set date
-const date = document.getAnimations('date');
-date.innerHtml = new Date().getFullYear();
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
 
-//close links
-const navToggle = document.querySelector(".nav-toggle");
-const linksContainer = document.querySelector(".links-container");
-const links = document.querySelector(".links");
+  const giveaway = document.querySelector(".giveaway");
+  const deadline = document.querySelector(".deadline");
+  const items = document.querySelectorAll(".deadline-format h4");
 
-navToggle.addEventListener('click', function(){
-    //linksContainer.classList.toggle("show-links");
-    const containerHeight = linksContainer.getBoundingClientRect().height;
-    const linksHeight = links.getBoundingClientRect().height;
-    console.log(linksHeight);
-    
-    if(containerHeight === 0){
-       linksContainer.style.height = `${linksHeight}px`;
-    }
-    else{
-        linksContainer.style.height = 0;
-    }
-});
-const navbar = document.getElementById("nav");
-const topLink = document.querySelector(".top-link");
-//fixed navbar
-window.addEventListener('click', function(){
-    const scrollHeight = window.pageYOffset;
-    const navHeight = navbar.getBoundingClientRect().height;
-    if(scrollHeight > navHeight){
-           navbar.classList.add("fixed-nav");  
-    }
-    else{
-           navbar.classList.remove("fixed-nav");
+  let tempDate = new Date();
+  let tempYear = tempDate.getFullYear();
+  let tempMonth = tempDate.getMonth();
+  let tempDay = tempDate.getDate();
+
+
+  const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+  
+  const year = futureDate.getFullYear();
+  const hours = futureDate.getHours();
+  const minutes = futureDate.getMinutes();
+
+  let month = futureDate.getMonth();
+  month = months[month]; 
+  const date = futureDate.getDate();
+  const weekday = weekdays[futureDate.getDate()];
+
+  giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+  //future time in miliseconds.
+  const futureTime = futureDate.getTime();
+ 
+  function getRemainingTime() {
+    const today = new Date().getTime();
+    const t = futureTime - today;
+   
+    //1s = 1000ms;
+    //1min = 60secs;
+    //1hr = 60mins;
+    //1day = 24hrs;
+
+    //value in ms.
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000; 
+    const oneMinute = 60 * 1000; 
+    //calculate all values.
+    let days = t / oneDay;
+    days = Math.floor(days);
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    let seconds = Math.floor((t % oneMinute) / 1000);
+    //set values array.
+    const values = [ days, hours, minutes, seconds];
+
+    function format(item){
+      if(item < 10 ){
+        return item = `0${item}`;
+      }
+      return item;
     }
 
-    if(scrollHeight > 500){
-        topLink.classList.add('show-link');
-    }
-    else{
-        topLink.classList.remove('show-link');
-    }
-});
-
-//select links.
-const scrollLinks = document.querySelectorAll(".scroll-link");
-    scrollLinks.forEach(function(link){
-       link.addEventListener("click", function(e){
-        //prevent default
-        e.preventDefault();
-        //navigate to specific spot
-           const id = e.currentTarget.getAttribute("href").slice(1);
-           const element = document.getElementById(id);
-           //calculate the heights.
-           const navHeight = navbar.getBoundingClientRect().height;
-           const containerHeight = linksContainer.getBoundingClientRect().height;
-           const fixedNav = navbar.classList.contains("fixed-nav");
-           
-           let position = element.offsetTop - navHeight;
-             if(!fixedNav){
-                position = position-navHeight;
-             }
-             if(navHeight > 82){
-                position = position + containerHeight;
-             }
-            window.scrollTo({
-            left: 0,
-            top: position,
-           });
-           linksContainer.style.height = 0;
-       });
+    items.forEach(function(item, index){
+      item.innerHTML = format(values[index]);
     });
+    if(t < 0){
+      clearInterval(countdown);
+      deadline.innerHTML = `<h4 class = "expired"> sorry, this giveaway has expired</h4>`;
+    }
+ 
+  }
+//countdown
+let countdown = setInterval(getRemainingTime, 1000);
+  getRemainingTime();
