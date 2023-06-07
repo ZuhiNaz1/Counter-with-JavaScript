dbca8f95e9ed3d79450b542981be723b561fb285
ZuhiNaz1 <zuhinaz1518@gmail.com>
2023-06-07 06:10:05 +0100

    final commit

--- app.js
+++ app.js
@@ -1,91 +1,191 @@
-const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-  const weekdays = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-  ];
+//select items.
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
 
-  const giveaway = document.querySelector(".giveaway");
-  const deadline = document.querySelector(".deadline");
-  const items = document.querySelectorAll(".deadline-format h4");
+//edit option.
+let editElement;
+let editFlag = false;
+let editID = "";
 
-  let tempDate = new Date();
-  let tempYear = tempDate.getFullYear();
-  let tempMonth = tempDate.getMonth();
-  let tempDay = tempDate.getDate();
+//Submit form.
+form.addEventListener("submit", addItem);
+//clear items.
+clearBtn.addEventListener("click", clearItems);
+// display items onload
+window.addEventListener("DOMContentLoaded", setUpItems);
+//functions.
+//add item.
+function addItem(e){
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
 
+ if(value !== "" && !editFlag){
+ const element = document.createElement("article");
+ let attr = document.createAttribute("data-id");
+  attr.value = id;
+   element.setAttributeNode(attr);
+   element.classList.add('grocery-item');
 
-  const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
-  
-  const year = futureDate.getFullYear();
-  const hours = futureDate.getHours();
-  const minutes = futureDate.getMinutes();
+   element.innerHTML = `<p class="title">${value}</p>
+   <div class="btn-container">
+    <button type="button" class="edit-btn">
+       <i class="fas fa-edit"></i>
+    </button>
+    <button type="button" class="delete-btn">
+      <i class="fas fa-trash"></i>
+   </button>
+   </div>`;
+   const deleteBtn = element.querySelector('.delete-btn');
+   deleteBtn.addEventListener("click", deleteItem);
+   const editBtn = element.querySelector('.edit-btn');
+   editBtn.addEventListener("click", editItem);
+   //append child
+   list.appendChild(element);
+   //display alert
+   displayAlert("item added to the list", "success");
+   //show container.
+   container.classList.add("show-container");
+   //add to local storage.
+   addToLocalStorage(id, value);
+   //set back to default
+   setBackToDefault();
+}
+ else if(value !== "" && editFlag){
+   editElement.innerHTML = value;
+   displayAlert("value changed", "success");
+   editLocalStorage(editID, value);
+   setBackToDefault();
+  }
+ else{
+   displayAlert("please enter value", "danger");
+}
+}
 
-  let month = futureDate.getMonth();
-  month = months[month]; 
-  const date = futureDate.getDate();
-  const weekday = weekdays[futureDate.getDate()];
+//display alert.
+function displayAlert(text, action){
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
 
-  giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+  //remove alert
+  setTimeout(function(){
+    alert.textContent = "";
+  alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
+//clear items
+function clearItems(){
+  const items = document.querySelectorAll('.grocery-item');
+  if(items.length > 0){
+    items.forEach(function(item){
+         list.removeChild(item);
+    });
+  }
+  container.classList.remove("show-container");
+  displayAlert("empty list", "danger");
+  setBackToDefault();
+  localStorage.removeItem("list");
+}
+//delete item
+function deleteItem(e){
+  const element = e.currentTarget.parentElement.parentElement;
+  const id = element.dataset.id;
+  list.removeChild(element);
+  if(list.children.length === 0){
+    container.classList.remove("show-container");
+  }
+  displayAlert("item removed", "danger");
+  setBackToDefault();
+  removeFromLocalStorage(id);
+}
 
-  //future time in miliseconds.
-  const futureTime = futureDate.getTime();
- 
-  function getRemainingTime() {
-    const today = new Date().getTime();
-    const t = futureTime - today;
-   
-    //1s = 1000ms;
-    //1min = 60secs;
-    //1hr = 60mins;
-    //1day = 24hrs;
-
-    //value in ms.
-    const oneDay = 24 * 60 * 60 * 1000;
-    const oneHour = 60 * 60 * 1000; 
-    const oneMinute = 60 * 1000; 
-    //calculate all values.
-    let days = t / oneDay;
-    days = Math.floor(days);
-    let hours = Math.floor((t % oneDay) / oneHour);
-    let minutes = Math.floor((t % oneHour) / oneMinute);
-    let seconds = Math.floor((t % oneMinute) / 1000);
-    //set values array.
-    const values = [ days, hours, minutes, seconds];
-
-    function format(item){
-      if(item < 10 ){
-        return item = `0${item}`;
-      }
+//edit item
+function editItem(e){
+  const element = e.currentTarget.parentElement.parentElement;
+  //set edit item
+  editElement = e.currentTarget.parentElement.previousElementSibling; 
+  //set form value
+  grocery.value = editElement.innerHTML;
+  editFlag = true;
+  editID = element.dataset.id;
+  submitBtn.textContent = "edit";
+}
+  //set back to default
+function setBackToDefault(){
+ grocery.value = "";
+ editFlag = false;
+ editID = "";
+ submitBtn.textContent = "submit";
+}
+//local storage.
+function addToLocalStorage(id, value){
+  const grocery = { id, value};
+  let items = getLocalStorage();
+  items.push(grocery);
+  localStorage.setItem("list", JSON.stringify(items));
+}
+function getLocalStorage(){
+  return localStorage.getItem("list")
+  ? JSON.parse(localStorage.getItem("list"))
+  : [];
+}
+function removeFromLocalStorage(id){
+  let items = getLocalStorage();
+  items = items.filter(function(item){
+    if(item.id !== id){
       return item;
     }
+  });
+  localStorage.setItem("list", JSON.stringify(items));
+}
 
-    items.forEach(function(item, index){
-      item.innerHTML = format(values[index]);
-    });
-    if(t < 0){
-      clearInterval(countdown);
-      deadline.innerHTML = `<h4 class = "expired"> sorry, this giveaway has expired</h4>`;
+function editLocalStorage(id, value){
+  let items = getLocalStorage();
+  items = items.map(function(item){
+    if(item.id === id){
+      item.value = value;
     }
- 
+    return item;
+  });
+  localStorage.setItem("list", JSON.stringify(items));
+}
+
+//setup items
+function setUpItems(){
+  let items = getLocalStorage();
+  if(items.length > 0){
+    items.forEach(function(item){
+      createListItem(item.id, item.value)
+    })
+    container.classList.add("show-container")
   }
-//countdown
-let countdown = setInterval(getRemainingTime, 1000);
-  getRemainingTime();
+}
+function createListItem(id, value){
+  const element = document.createElement("article");
+  let attr = document.createAttribute("data-id");
+   attr.value = id;
+    element.setAttributeNode(attr);
+    element.classList.add('grocery-item');
+    
+   element.innerHTML = `<p class="title">${value}</p>
+   <div class="btn-container">
+    <button type="button" class="edit-btn">
+       <i class="fas fa-edit"></i>
+    </button>
+    <button type="button" class="delete-btn">
+      <i class="fas fa-trash"></i>
+   </button>
+   </div>`;
+   const deleteBtn = element.querySelector('.delete-btn');
+   deleteBtn.addEventListener("click", deleteItem);
+   const editBtn = element.querySelector('.edit-btn');
+   editBtn.addEventListener("click", editItem);
+
+//append child.
+list.appendChild(element);
+}
